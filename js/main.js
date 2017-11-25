38e0e6d00cad8d5841e39946d863ab4bec32b76e
Christopher Tran <christopher.long.tran@gmail.com>
2017-11-25 08:13:08 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,7 +2,28 @@
 
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
 
@@ -19,8 +40,11 @@ var checkForMatch = function(){
 
 var flipCard = function(cardId){
 
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log("User flipped " + cards[cardId].cardImage);
+console.log("User flipped " + cards[cardId].suit);
+
+cardsInPlay.push(cards[cardId].rank);
 
 checkForMatch();
 
